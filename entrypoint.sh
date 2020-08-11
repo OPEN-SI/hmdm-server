ebc2c5a954462af79ab3f739aa0376649eac90c9
Shadaï ALI <nafiouali@gmail.com>
2020-08-11 15:14:16 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 pg_ctlcluster 11 main start
-sudo -Hu postgres psql -U postgres -c "CREATE USER hmdm WITH PASSWORD 'topsecret';CREATE DATABASE hmdm WITH OWNER=hmdm;" postgres
+psql -U postgres -c "CREATE USER hmdm WITH PASSWORD 'topsecret';CREATE DATABASE hmdm WITH OWNER=hmdm;" postgres
 bash hmdm_install.sh
-bash $CATALINA_HOME/catalina.sh run 
-
