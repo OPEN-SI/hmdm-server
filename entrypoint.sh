be473267416747b54bf69facda6a378a57d28151
Shadaï ALI <nafiouali@gmail.com>
2020-08-11 15:16:11 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-service postgres start 
+service postgresql start 
 psql -U postgres -c "CREATE USER hmdm WITH PASSWORD 'topsecret';CREATE DATABASE hmdm WITH OWNER=hmdm;" postgres
 bash hmdm_install.sh
